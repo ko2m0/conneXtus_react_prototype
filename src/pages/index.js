ddebccc88fbf91d7c3b3ce7a5b0f5717f292aa5f
Ko2m0 <kozmopimp@gmail.com>
2022-08-16 23:08:05 -0500

    a wrapper

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,20 @@ import SmSection from '../components/SmSection'
 import TabsSection from '../components/TabsSection/index'
 import UserProfileSection from '../components/UserProfileSection'
 import VideoSection from '../components/VideoSection'
+import '../App.css'
 
 
 const Home = () => {
   return (
     <>
+    <div className='wrapper'>
     <VideoSection/>
     <UserProfileSection/>
     <BtnsSection/>
     <TabsSection/>
     <SmSection/>
     <Footer/>
+    </div>
     </>
   )
 }
